87dd13097f30a4ad00aa8ae4358d4b42d5a05965
Tanpero <3185750139@qq.com>
2019-04-29 20:41:15 +0800

    update some functions

--- editor.js
+++ editor.js
@@ -1,3 +1,4 @@
+
 class Editor {
     constructor (element, editorModel) {
         const _editor = element;
@@ -16,6 +17,52 @@ class Editor {
 
     setUnderline () {
         this.editorModel.setBasicStyle("underline");
+        return this;
+    }
+
+    setStrike () {
+        this.editorModel.setBasicStyle("strikeThrough");
+        return this;
+    }
+
+    setSubscript () {
+        this.editorModel.setBasicStyle("subscript");
+        return this;
+    }
+
+    setSuperscript () {
+        this.editorModel.setBasicStyle("superscript");
+        return this;
+    }
+
+    setFontColor (colorName) {
+        this.editorModel.setFontColor(colorName);
+        return this;
+    }
+
+    setBackgroundColor (colorName) {
+        this.editorModel.setBackgroundColor(colorName);
+        return this;
+    }
+
+    setHeading (level) {
+        this.editorModel.setHeading(level);
+        return this;
+    }
+
+    increaseFontSize () {
+        this.editorModel.increaseFontSize();
+        return this;
+    }
+
+    decreaseFontSize () {
+        this.editorModel.decreaseFontSize();
+        return this;
+    }
+
+    justify (type) {
+        this.editorModel.justify(type);
+        return this;
     }
 };
 
--- editorModel.js
+++ editorModel.js
@@ -1,35 +1,81 @@
+const utilities = require("./utilities");
+
 const editorModel = {
-    setBasicStyle(name) {
-        const styleList = editorRecords.basicStyleList;
+    setBasicStyle (name) {
+		const styleList = ["bold", "italic", "underline", "strikeThrough",
+								"subscript", "superscript"];
         if (styleList.includes(name)) {
             document.execCommand(name);
         } else {
             throw "Undefined Style Name";
         }
-    },
-	
-	setHeading (heading)  {
-		let formatTag  =  heading,
-			formatBlock  =  document.queryCommandValue("formatBlock");
-		if  (formatBlock.length  >  0  &&  formatBlock.toLowerCase()  ===  formatTag)  {
-			document.execCommand('formatBlock',  false,  ``);
-		}  else  {
-			document.execCommand('formatBlock',  false,  ``);
+	},
+
+	setHeading (heading) {
+		if (heading < 1 || heading > 6) {
+			throw "Unsupported Heading Level";
+		}
+		document.execCommand("formatBlock", false, "h" + heading);
+	},
+
+	setFontColor (colorName) {
+		document.execCommand("foreColor", false, colorName);
+	},
+
+	setBackgroundColor (colorName) {
+		document.execCommand("hiliteColor", false, colorName);
+	},
+
+	increaseFontSize () {
+		document.execCommand("increaseFontSize");
+	},
+
+	decreaseFontSize () {
+		document.execCommand("decreaseFontSize");
+	},
+
+	insertHorizontalRule () {
+		document.execCommand("insertHorizontalRule");
+	},
+
+	insertHTML (html) {
+		document.execCommand("insertHTML", false, html);
+	},
+
+	justify (type) {
+
+		/*
+		* justifyRight: 光标插入位置或者所选内容 右对齐
+		* justifyLeft: 光标插入位置或者所选内容 左对齐
+		* justifyFull: 光标插入位置或者所选内容 文本对齐
+		* justifyCenter: 光标插入位置或者所选内容 文本居中
+		*/
+
+		let rightType = {
+			"right":  "Right",
+			"left":   "Left",
+			"full":   "Full",
+			"center": "Center",
+		}[type];
+		if (!rightType) {
+			throw "Unsupported Justifying Type";
+		} else {
+			document.execCommand("justify" + rightType);
 		}
 	},
-	
-	adjustList() {
-		let lists  =  document.querySelectorAll("ol, ul");
-		for  (let  i  =  0;  i  <  lists.length;  i++)  {
-			let ele  =  lists[i];  // ol
-			let parentNode  =  ele.parentNode;	
-			if  (parentNode.tagName  ===  'P'  &&  parentNode.lastChild  ===  parentNode.firstChild)  {
-					parentNode.insertAdjacentElement('beforebegin',  ele);
-					parentNode.remove()
+
+	adjustList () {
+		let lists = document.querySelectorAll("ol, ul");
+		for  (let i = 0; i < lists.length; i++)  {
+			let ele = lists[i]; // ol
+			let parentNode = ele.parentNode;
+			if  (parentNode.tagName === 'P' && parentNode.lastChild === parentNode.firstChild) {
+					parentNode.insertAdjacentElement('beforebegin', ele);
+					parentNode.remove();
 			}
 		}
 	},
-	
+
 	insertLink (url, title) {
 		let selection = document.getSelection(),
 			range = selection.getRangeAt(0);
@@ -45,22 +91,22 @@ const editorModel = {
 			document.execCommand('createLink', false, url);
 		}
 	},
-	
-	findParentByTagName(root,  name)  {
-		let parent  =  root;
-		if  (typeof name  ===  "string")  {
-			name  =  [name];
+
+	findParentByTagName (root, name)  {
+		let parent = root;
+		if  (typeof name === "string")  {
+			name = [name];
 		}
-		while  (name.indexOf(parent.nodeName.toLowerCase())  ===  -1  &&
-				parent.nodeName  !==  "BODY"  &&  parent.nodeName  !==  "HTML")  {
-			parent  =  parent.parentNode;
+		while (name.indexOf(parent.nodeName.toLowerCase()) === -1 &&
+				parent.nodeName !== "BODY" && parent.nodeName !== "HTML")  {
+			parent = parent.parentNode;
 		}
-		return  parent.nodeName  ===  "BODY"  ||
-			parent.nodeName  ===  "HTML"  ?  null  :  parent;
+		return parent.nodeName === "BODY" ||
+			parent.nodeName === "HTML" ? null : parent;
 
 	},
-	
-    insertText (eventObject, withFormat) {
+
+    pasteText (eventObject, withFormat) {
         let plainText = eventObject.clipboardData.getData('text/plain');  // 无格式文本
         let plainHTML = eventObject.clipboardData.getData('text/html'); // 有格式文本
 
